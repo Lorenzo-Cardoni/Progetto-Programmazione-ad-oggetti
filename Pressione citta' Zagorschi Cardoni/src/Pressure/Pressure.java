29bb43160108db9d37ded242671951e7aa60003a
Lorenzo-Cardoni <cardonilorenzo31@users.noreply.github.com>
2020-12-09 09:22:25 +0100

    Update Pressure.java

--- Pressione citta' Zagorschi Cardoni/src/Pressure/Pressure.java
+++ Pressione citta' Zagorschi Cardoni/src/Pressure/Pressure.java
@@ -1,5 +1,25 @@
 package Pressure;
 
 public class Pressure {
+    private float value;
+    private float time;
+    private String nameCity;
 
+    public Pressure(float valuePressure, float time, String nameCity){
+        this.value = valuePressure;
+        this.time = time;
+        this.nameCity = nameCity;
+    }
+
+    public float getValue(){
+        return this.value;
+    }
+
+    public float getTime(){
+        return this.time;
+    }
+
+    public String getNameCity(){
+        return this.nameCity;
+    }
 }
