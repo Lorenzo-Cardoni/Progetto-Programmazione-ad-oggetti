3f935b470889ae03c4a826fe2c1ef1773256e7c5
Lorenzo-Cardoni <cardonilorenzo31@users.noreply.github.com>
2020-12-09 09:22:29 +0100

    Update City.java

--- Pressione citta' Zagorschi Cardoni/src/Pressure/City.java
+++ Pressione citta' Zagorschi Cardoni/src/Pressure/City.java
@@ -1,7 +1,25 @@
 package Pressure;
 
 public class City {
-    private int cityname;
-    private float pressure;
-    
+    private String cityname;
+    private String region;
+    private int postalCode;
+
+    public City(String cityname, String nameregion, int postalCode){
+        this.cityname = cityname;
+        this.region = nameregion;
+        this.postalCode = postalCode;
+    }
+
+    public String getCityname(){
+        return this.cityname;
+    }
+
+    public String getRegion(){
+        return this.region;
+    }
+
+    public int getPostalCode(){
+        return this.postalCode;
+    }
 }
