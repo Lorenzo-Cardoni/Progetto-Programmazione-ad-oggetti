1c9e56fd9c00b35cb7801be1eeec9a1088ec1f79
francozagorschi <francozagorschi@gmail.com>
2020-12-11 14:33:18 +0100

    Update Statistics.java

--- Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
+++ Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
@@ -1,5 +1,18 @@
 package Pressure;
 
 public class Statistics {
+    private String nameCity;
+    private String duration;
+    private float val_min;
+    private float val_max;
+    private float average;
+    private float variance;
+
+    public Statistics(){};
+
+    public Statistics(String nameCity,String duration){
+        this.duration=duration;
+        this.nameCity=nameCity;
+    };
 
 }
