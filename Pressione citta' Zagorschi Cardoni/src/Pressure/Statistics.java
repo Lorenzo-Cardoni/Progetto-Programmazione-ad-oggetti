d79a007cc9e954d4f6e48c285c6cc10e823ca02e
Lorenzo-Cardoni <cardonilorenzo31@users.noreply.github.com>
2020-12-11 17:43:59 +0100

    Update Statistics.java

--- Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
+++ Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
@@ -11,18 +11,22 @@ public class Statistics {
 
     public Statistics(){};
 
-    public Statistics(String nameCity,String duration, Pressure[] pressure){
-        this.duration=duration;
-        this.nameCity=nameCity;
-        this.pressure=pressure;
-    };
-    void createStatistics(){
-        for(int i=0;i<this.pressure.length;i++){
-            if(pressure.value[i]>val_max)val_max=pressure.value[i];
-            if(pressure.value[i]<val_min)val_min=pressure.value[i];
+    public Statistics(String nameCity , String duration , Pressure[] pressure){
+        this.duration = duration;
+        this.nameCity = nameCity;
+        this.pressure = pressure;
+    }
+
+    public void createStatistics(){
+        for(int i=0 ; i < this.pressure.length ; i++){
+            if(pressure.value[i] > val_max)
+                val_max=pressure.value[i];
+            if(pressure.value[i]<val_min)
+                val_min=pressure.value[i];
             average+=pressure.value[i];
         }
-        average/=pressure.length;
+        average /= pressure.length;
+
         for(int i=0;i<this.pressure.length;i++){
             variance+=((pressure.value[i]-average)*(pressure.value[i]-average))/average;
         }
