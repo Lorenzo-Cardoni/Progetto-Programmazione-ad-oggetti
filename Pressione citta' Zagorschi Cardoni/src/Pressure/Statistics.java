e359d27c58c521aa57a6fba9fe5a7ff68171cff8
francozagorschi <francozagorschi@gmail.com>
2020-12-11 14:42:53 +0100

    Update Statistics.java

--- Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
+++ Pressione citta' Zagorschi Cardoni/src/Pressure/Statistics.java
@@ -3,6 +3,7 @@ package Pressure;
 public class Statistics {
     private String nameCity;
     private String duration;
+    private Pressure[] pressure;
     private float val_min;
     private float val_max;
     private float average;
@@ -10,9 +11,17 @@ public class Statistics {
 
     public Statistics(){};
 
-    public Statistics(String nameCity,String duration){
+    public Statistics(String nameCity,String duration, Pressure[] pressure){
         this.duration=duration;
         this.nameCity=nameCity;
+        this.pressure=pressure;
     };
-
+    void createStatistics(){
+        for(int i=0;i<this.pressure.length;i++){
+            if(pressure.value>val_max)val_max=pressure.value;
+            if(pressure.value<val_min)val_min=pressure.value;
+            average+=pressure.value;
+        }
+        average/=pressure.length;
+    }
 }
