c8c3ffd84e9985a92ac47ec703b9fed35ce60c9f
franco zagorschi <francozagorschi@gmail.com>
2020-12-17 17:55:55 +0100

    modifica classi api e json

--- Pressione citta' Zagorschi Cardoni/src/Service/Api.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Api.java
@@ -1,15 +1,23 @@
 package Service;
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Api {
-    public Api(){}
-    public Api(String cityName){
+    public Api() throws MalformedURLException {}
+    public Api(String cityName) throws MalformedURLException {
         this.cityName=cityName;
     }
     String cityName;
     static String apiKey="39b2f77fcfc40aa96026fc4d80eb9bb0";
     URL url = new URL("http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=" + apiKey);
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
+    HttpURLConnection con;
+    {
+        try {
+            con = (HttpURLConnection) url.openConnection();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
 }
