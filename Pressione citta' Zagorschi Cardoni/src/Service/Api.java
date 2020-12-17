96143e1147e060fece5926738ec69647177def8f
franco zagorschi <francozagorschi@gmail.com>
2020-12-17 17:11:17 +0100

    Aggiunta classi api e json

--- Pressione citta' Zagorschi Cardoni/src/Service/Api.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Api.java
@@ -1,11 +1,11 @@
 package Service;
 
 public class Api {
-    static string apiKey="";
-    string url = "" + apiKey;
-    string cityName;
-    public Api(){};
-    public Api(string cityName){
+    static String apiKey="39b2f77fcfc40aa96026fc4d80eb9bb0";
+    String url = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=" + apiKey;
+    String cityName;
+    public Api(){}
+    public Api(String cityName){
         this.cityName=cityName;
     }
 }
