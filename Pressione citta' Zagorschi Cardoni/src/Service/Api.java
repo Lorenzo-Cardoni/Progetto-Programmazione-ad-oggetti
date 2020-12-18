5a39d957a59fa53691d7423570b8bcd55b709ced
franco zagorschi <francozagorschi@gmail.com>
2020-12-18 18:00:23 +0100

    Chiamata api funzionante

--- Pressione citta' Zagorschi Cardoni/src/Service/Api.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Api.java
@@ -1,36 +1,44 @@
 package Service;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.http.HttpResponse;
 
 public class Api {
     public Api() throws MalformedURLException {}
     //Variables
-    String cityName="q=";
+    String cityName;
     float pression;
     public boolean done;
     static String apiKey="&appid=39b2f77fcfc40aa96026fc4d80eb9bb0";
     URL url = null;
     HttpURLConnection con;
-
+    String data;
     //Methods
 
     //Call api knowing city name
-    public boolean callApi(String cityName){
-        this.cityName += cityName;
+    public String callApi(String city){
+        this.cityName = "q=" + city;
         try {
             url = new URL("http://api.openweathermap.org/data/2.5/weather?"+cityName+apiKey);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
             con = (HttpURLConnection) url.openConnection();
-            done=true;
-        } catch (IOException ioException) {
+            if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
+                InputStream inputStream = con.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    data+=inputLine;
+                in.close();
+            }
+        }
+        catch (IOException ioException) {
             ioException.printStackTrace();
         }
-        return done;
+        return data;
     }
 
     //Set city name
