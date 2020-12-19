5fa7556ae903aaea3ebf1d2bd9f5a46d51442175
Lorenzo-Cardoni <cardonilorenzo31@users.noreply.github.com>
2020-12-19 17:21:01 +0100

    Aggiunto commenti alla classe filtroStandard.

--- Pressione citta' Zagorschi Cardoni/src/Filters/FilterPressureStandard.java
+++ Pressione citta' Zagorschi Cardoni/src/Filters/FilterPressureStandard.java
@@ -45,24 +45,50 @@ public class FilterPressureStandard implements Filter {
         this.startPeriod = LocalDate.parse(date);
     }
 
-
+    /**
+     *  Questa classe filtra le pressioni in base al nome della citta' e alla data.
+     *
+     * @param pressures vettore in input che contiene tutti i valori delle pressioni.
+     * @return In output ottengo una stringa con i valori delle statistiche
+     */
     @Override
     public String filtersPressure(Vector<Pressure> pressures) {
 
+        /**
+         * Ciclo for che mi sfoglia tutto il vettore contenente le pressioni non filtrate.
+         */
         for(int i=0; i < pressures.size(); i++){
 
+            /**
+             * localDate attributo che mi prende la data da cui inizia il periodo.
+             */
             LocalDate localDate = LocalDate.parse(pressures.get(i).getDate());
 
+            /**
+             *  Ciclo if che mi permette di filtrare le pressioni in base al nome e alla data.
+             *  Si e' utilizzato un metodo della libreria time di java.
+             */
             if(pressures.get(i).getNameCity() == cityName && localDate.isAfter(startPeriod))
                 this.pressureFiltred.add(pressures.get(i));
 
         }
 
+        /**
+         * crea un riferimento alla classe dove risiedono i metodi delle statistiche.
+         *
+         */
         Stats statistics = new Stats(this.pressureFiltred);
 
+        /**
+         * crea le varie statistiche.
+         *
+         */
         statistics.createStats();
 
-
+        /**
+         * Output di tipo stringa composto dai metodi della classe Stats.
+         *
+         */
         return "Minimum value is "+statistics.getVal_min()+"\nMaximum value is "+statistics.getVal_max()+
                 "\nAvarege is "+statistics.getAverage()+"\nVariance is "+statistics.getVariance();
     }
