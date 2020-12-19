fbd4776fd8ec9024475eedef2748bc667903024d
franco zagorschi <francozagorschi@gmail.com>
2020-12-19 19:04:35 +0100

    Classe Json funzionante

--- Pressione citta' Zagorschi Cardoni/src/Service/Json.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Json.java
@@ -1,15 +1,16 @@
 package Service;
-import org.json.*;
 import java.io.IOException;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
 
 public class Json   {
+    String pressure, city;
     public Json(){}
-    String data="";
     public String getPressure(String info){
-        data=info;
-        JSONObject obj = new JSONObject(data);
-        JSONObject oTime = obj.getJSONObject ("pressure");
-        return oTime.toString();
+        JsonObject jsonObject = new JsonParser().parse(info).getAsJsonObject();
+        pressure = jsonObject.getAsJsonObject("main").get("pressure").getAsString();
+        city = jsonObject.get("name").getAsString();
+        return "Citta': " + city + " Pressione: " + pressure;
     }
 }
