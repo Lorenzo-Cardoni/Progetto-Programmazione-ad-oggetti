403888127173f986046f2f09e253c5a789ed167c
franco zagorschi <francozagorschi@gmail.com>
2020-12-22 21:45:48 +0100

    Aggiornamento json

--- Pressione citta' Zagorschi Cardoni/src/Service/Json.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Json.java
@@ -5,21 +5,32 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static java.lang.Float.parseFloat;
 
 public class Json   {
     String city;
     float pressure;
     Pressure objectPressure = new Pressure();
-    Gson gson=new Gson();
+    Gson gson = new Gson();
     public Json(){}
     public String getPressure(String info){
+        //
         JsonObject jsonObject = new JsonParser().parse(info).getAsJsonObject();
-        pressure =parseFloat(jsonObject.getAsJsonObject("main").get("pressure").getAsString());
+        pressure = parseFloat(jsonObject.getAsJsonObject("main").get("pressure").getAsString());
         city = jsonObject.get("name").getAsString();
         objectPressure.value=pressure;
         objectPressure.nameCity=city;
-        //gson.toJson(objectPressure, new FileWriter(filePath));
-        return "Citta': " + city + " Pressione: " + pressure;
+        //
+        try {
+            FileWriter writer = new FileWriter("Output.json");
+            new Gson().toJson(jsonObject, writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "AAAAAAAAAAAAAAA";
     }
 }
