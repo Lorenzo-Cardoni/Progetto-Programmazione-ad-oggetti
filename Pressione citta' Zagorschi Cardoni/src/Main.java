05b1fb613c41b42c171316af7f9a63a67dc52b6c
francozagorschi <francozagorschi@gmail.com>
2021-01-01 22:23:18 +0100

    .

--- Pressione citta' Zagorschi Cardoni/src/Main.java
+++ Pressione citta' Zagorschi Cardoni/src/Main.java
@@ -1,17 +1,18 @@
 import Database.ReadFile;
+import Service.Api;
 import Utils.ConvertHashMapToVector;
 
 public class Main {
     public static void main(String[] args) {
         ReadFile readFile = new ReadFile();
+        Api api=new Api();
         ConvertHashMapToVector convert = new ConvertHashMapToVector();
-        convert.convertHashMapToVector(readFile.readFile("Pesaro"));
         /**
          * sistemare eccezione file inesistente per read file
          */
-        //Api api=new Api();
-        //api.callApi("Pesaro");
-        System.out.print("Operazion finite");
+        api.callApi("Pesaro");
+        convert.convertHashMapToVector(readFile.readFile("Pesaro"));
+        System.out.print(" Operazioni finite ");
         //SelectionCity window1 = new SelectionCity("City Name");
     }
 }
--- Pressione citta' Zagorschi Cardoni/src/Service/Api.java
+++ Pressione citta' Zagorschi Cardoni/src/Service/Api.java
@@ -21,7 +21,7 @@ public class Api {
     static String apiKey = "&appid=39b2f77fcfc40aa96026fc4d80eb9bb0";
     public void callApi(String city) {
         this.cityName = "q=" + city;
-        try {
+         try {
             URL url = new URL("http://api.openweathermap.org/data/2.5/weather?"+cityName+apiKey);
             con = (HttpURLConnection) url.openConnection();
             if(con.getResponseCode() == HttpURLConnection.HTTP_OK){
